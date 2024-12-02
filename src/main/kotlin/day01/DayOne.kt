f96430ad2fca247bc44a062373cdeca0408ab337
Brendan Holly <bholly24@gmail.com>
2024-12-02 07:04:48 -0500

    Refactor day one

--- src/main/kotlin/day01/DayOne.kt
+++ src/main/kotlin/day01/DayOne.kt
@@ -4,82 +4,52 @@ import java.io.File
 import kotlin.math.abs
 
 class DayOne(filePath: String) {
-    private val reports: List<MutableList<Int>>
-
-    init {
-        reports = File(filePath).readLines()
-            .map { it.split(Regex("\\s+")).map(String::toInt).toMutableList() }
-    }
+    private val reports: List<MutableList<Int>> = File(filePath).readLines()
+        .map { it.split(Regex("\\s+")).map(String::toInt).toMutableList() }
 
     fun partA(): Int {
-        val count = getTotalValidReports(reports)
-        println("Count is $count")
+        val count = reports.sumOf { if (isReportValid(it)) 1.toInt() else 0 }
+        println("Total safe reports is $count")
         return count
     }
 
     fun partB(): Int {
-        var count = 0
-
-        for (index in reports.indices) {
-            val report = reports[index]
-            var isPositive: Boolean? = null
-            for (i in report.indices) {
-                if (i > 0) {
-                    val isBad = violatesRules(report[i], report[i - 1], isPositive)
-                    if (isBad) {
-                        val badLists = mutableListOf<List<Int>>()
-                        for (badI in report.indices) {
-                            badLists.add(report.filterIndexed { myI, _ -> badI != myI })
-                        }
-                        if (getTotalValidReports(badLists) > 0) {
-                            count++
-                        }
-                        break
-                    } else {
-                        isPositive = report[i] - report[i - 1] > 0
-                    }
-                    if (i == report.size - 1) {
-                        count++
-                    }
-                }
+        val count = reports.sumOf {
+            if (isReportValid(it)) {
+                1.toInt()
+            } else {
+                val badLists: List<List<Int>> = it
+                    .mapIndexed { index, i -> it.filterIndexed { myI, _ -> index != myI }}
+                if (badLists.any { r -> isReportValid(r) }) 1 else 0
             }
         }
 
-        println("Total count is $count")
+        println("Total count with problem dampener is $count")
         return count
     }
 
     private fun violatesRules(item: Int, priorItem: Int, priorSign: Boolean?): Boolean {
         val diff = item - priorItem
         val isPositive = diff > 0
-        if ((priorSign != null && priorSign != isPositive) || abs(diff) < 1 || abs(diff) > 3) {
-            return true
-        } else {
-            return false
-        }
+        return (priorSign != null && priorSign != isPositive) || abs(diff) < 1 || abs(diff) > 3
     }
 
-    private fun getTotalValidReports(reports: List<List<Int>>): Int {
-        var count = 0
+    private fun isReportValid(report: List<Int>): Boolean {
+        var isPositive: Boolean? = null
 
-        for (index in reports.indices) {
-            var report = reports[index]
-            var isPositive: Boolean? = null
-
-            for (i in report.indices) {
-                if (i > 0) {
-                    val violates = violatesRules(report[i], report[i - 1], isPositive)
-                    if (violates) {
-                        break
-                    } else {
-                        isPositive = report[i] - report[i - 1] > 0
-                    }
-                    if (i == report.size - 1) {
-                        count++
-                    }
+        for (i in report.indices) {
+            if (i > 0) {
+                val violates = violatesRules(report[i], report[i - 1], isPositive)
+                if (violates) {
+                    break
+                } else {
+                    isPositive = report[i] - report[i - 1] > 0
+                }
+                if (i == report.size - 1) {
+                    return true
                 }
             }
         }
-        return count
+        return false
     }
 }
